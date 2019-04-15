d19d94aff029052b39346fb8f458eea8940b8edd
Cyril MERY <cmery@coffreo.com>
2019-04-15 17:45:32 +0200

    feat(header): Add generic license header
    This header don't set the license type, it just indicate that license is located
    in LICENSE file at project root.

---
+++ resources/headers/license.php
@@ -0,0 +1,9 @@
+<?php
+return <<<HEADER
+This file is part of Coffreo project "$composer->name"
+
+(c) Coffreo SAS <contact@coffreo.com>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+HEADER;
