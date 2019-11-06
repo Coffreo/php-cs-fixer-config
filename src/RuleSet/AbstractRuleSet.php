1cd23e8052ce550ec646e5cd80cce5cc2263f60c
Cyril MERY <cmery@coffreo.com>
2019-11-06 09:49:57 +0100

    cs: apply coding standard

--- src/RuleSet/AbstractRuleSet.php
+++ src/RuleSet/AbstractRuleSet.php
@@ -45,17 +45,11 @@ abstract class AbstractRuleSet implements RuleSet
         }
 
         if (!\is_string($header)) {
-            throw new \InvalidArgumentException(\sprintf(
-                'Header needs to be specified as null or a string. Got "%s" instead.',
-                \is_object($header) ? \get_class($header) : \gettype($header)
-            ));
+            throw new \InvalidArgumentException(\sprintf('Header needs to be specified as null or a string. Got "%s" instead.', \is_object($header) ? \get_class($header) : \gettype($header)));
         }
 
         if ('' === \trim($header)) {
-            throw new \InvalidArgumentException(\sprintf(
-                'If specified, header needs to be a non-blank string. Got "%s" instead.',
-                $header
-            ));
+            throw new \InvalidArgumentException(\sprintf('If specified, header needs to be a non-blank string. Got "%s" instead.', $header));
         }
 
         $this->rules['header_comment'] = [
